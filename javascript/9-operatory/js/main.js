24f5c34836f3b6e750624304995c0ab2dc2040ab
Adrian Lewandowski <lewandow.adr88@gmail.com>
2019-07-08 15:00:18 +0200

    Create main.js

---
+++ javascript/9-operatory/js/main.js
@@ -0,0 +1,23 @@
+//inkr/dekrementacja
+let liczba = 4;
+console.log(liczba++); //5
+console.log(++liczba);
+
+let liczba2 = 10;
+liczba2 += liczba2;
+//zapis równoznaczny
+//liczba2 = liczba2 + liczba 2
+console.log(liczba2);
+//LS = LS operator PS
+let liczba1 = 5;
+console.log (liczba1);
+liczba1 -= liczba2;
+console.log(liczba2);
+
+if('2' == 2) {
+    console.log('sprawdzam wartości');
+}
+
+if('2' == 2 || '3' === 3) {
+    console.log('prawda');
+}
