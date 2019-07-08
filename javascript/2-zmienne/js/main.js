b2957786d071ac9ca3a99c1c47c1be9a1e2a118a
Adrian Lewandowski <lewandow.adr88@gmail.com>
2019-07-08 09:25:37 +0200

    Create main.js

---
+++ javascript/2-zmienne/js/main.js
@@ -0,0 +1,10 @@
+// tworzenie zmiennej
+// deklaracja zmiennej
+let x;
+// inicjalizacja zmiennej
+let napis = 'Marcin';
+// stała
+const drugiNapis = 'Michał';
+
+console.log(napis);
+console.log(drugiNapis);
