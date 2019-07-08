db0ba3fbb06bdddb44b875d06fbf32a042e07fd3
Adrian Lewandowski <lewandow.adr88@gmail.com>
2019-07-08 12:26:25 +0200

    Create main.js

---
+++ javascript/4-zasięg zmiennych/js/main.js
@@ -0,0 +1,11 @@
+// deklaracja zmiennej NA GÓRZE LUB PRZED DEF. FUNKCJI
+let wynik;
+
+const dodawanie = (par1, par2) => {
+    let result = par1 + par2;
+    return result;
+}
+
+//console.log(result);
+wynik = dodawanie (1,2)
+console.log(wynik)
