d77e60c223229f02da7f4f4daf34facef294df91
Adrian Lewandowski <lewandow.adr88@gmail.com>
2019-07-08 09:53:53 +0200

    Create main.js

---
+++ javascript/3-funkcje/js/main.js
@@ -0,0 +1,8 @@
+console.log('podpiete');
+// funkcja to nazwany blok kodu, który wykonuje zadane instrukcje po wywołaniu
+// definicja funkcji za pomocą słowa kluczowego "function"
+function zalogujDoKonsoli() {
+    console.log('podpiete');
+}
+// wywołanie funkcji - napisanie nazwy
+zalogujDoKonsoli();
