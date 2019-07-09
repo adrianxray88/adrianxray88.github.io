32fb1bb9b0127f257ca35ae894572cb2444de524
Adrian Lewandowski <lewandow.adr88@gmail.com>
2019-07-09 15:55:25 +0200

    Update main.js

--- DOM/6-zdarzenia/js/main.js
+++ DOM/6-zdarzenia/js/main.js
@@ -1,3 +1,29 @@
-document.getElementById('id').addEventListener('co', functioun()) {
+const showText = () => {
+    event.preventDefault();
+    console.log(event);
+    console.log('kliknieto');
+}
 
+const changeBackground = (event) => {
+    console.log(event);
+    document.body.style.backgroundColor = "grey";
+}
+
+// const changeBackground = (event) => {
+//     if()
+//     document.body.style.backgroundColor = "white";
+
+document.body.addEventListener('keydown', changeBackground);
+let firstLink = document.getElementsByClassName('link')[4];
+// firstLink.onclick = showText(event);
+
+firstLink.addEventListener('click', showText);
+
+// firstLink.addEventListener('click', function(e) {
+//     event.preventDefault();
+//     console.log('kliknieto');
+// }
+let container = document.getElementById('sectionFirst');
+container.addEventListener('click', (e) => {
+    console.log(e.target);
 })
