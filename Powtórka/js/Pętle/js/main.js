7016430c4fcb0f2c0fe9ce6e52ac006e4f7dd32c
Adrian Lewandowski <lewandow.adr88@gmail.com>
2019-07-09 09:35:02 +0200

    Update main.js

---
+++ Powtórka/js/Pętle/js/main.js
@@ -0,0 +1,23 @@
+let tablica = [1, 20, 10, 59, 214, 123];
+
+for(let i = 0; i <)
+
+let nazwaObiektu = {
+    nazwa: 'obiekt',
+    'typ obiektu': 'obiekt'
+}
+
+for(let asd in nazwaObiektu) {
+    console.log(nazwaObiektu[asd]);
+}
+
+let number = 0;
+while(number < 2) {
+    console.log(++number);
+}
+
+let iterator = 0;
+do {
+    console.log(iterator);
+    iterator++;
+} while(iterator < 0);
