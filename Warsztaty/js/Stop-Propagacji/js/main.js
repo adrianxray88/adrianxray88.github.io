fbcdae7e48d35c379fc9cd785ed5b481d224192c
Adrian Lewandowski <lewandow.adr88@gmail.com>
2019-07-09 15:55:32 +0200

    Create main.js

---
+++ Warsztaty/js/Stop-Propagacji/js/main.js
@@ -0,0 +1,28 @@
+let btn = document.getElementsByClassName('btn')[0];
+let mainNav = document.getElementsByClassName('main-nav')[0];
+
+btn.addEventListener('click', () => {
+    e.stopPropagation();
+    mainNav.classList.toggle('toggled');
+});
+
+document.body.addEventListener('click', () => {
+    mainNav.classList.remove('toggled');
+})
+
+mainNav.addEventListener('click', (e) => {
+    e.stopPropagation();
+});
+
+let nodeLista = document.querrySelectorAll('.list-item a');
+
+for(let i = 0; i < nodeLista.length; i++){
+    console.log(nodeLista[i]);
+    nodeLista[i].addEventListener('click', (e) => {
+        e.preventDefault();
+    });
+}
+
+// document.querySelectorAll('.list-item a').addEventListener('click', (e) => {
+//     e.preventDeafault();
+// })
