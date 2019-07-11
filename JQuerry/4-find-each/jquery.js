f6da32c59d33247953efbf46a836c11398db31c3
Adrian Lewandowski <lewandow.adr88@gmail.com>
2019-07-11 14:51:50 +0200

    Update jquery.js

--- JQuerry/4-find-each/jquery.js
+++ JQuerry/4-find-each/jquery.js
@@ -7,3 +7,31 @@ $(function(){
         console.log(jQuery(this));
     })
 })
+
+$(function(){
+    'use strict';
+    //efekt hid ei show
+    $('p').hide(3000).show(3000);
+    $('h1').fadeOut(3000).fadeIn(3000);
+    $('h2').slideUp(3000).slideDown(3000); 
+    $('h3').animate(
+        {'font-size': '3em', 'margin-left': '100px'},
+        2000
+    );
+
+
+    function poZakonczonejAnimacji() {
+        console.log("zakończono pierwszą animacje. Zaraz startuje druga :)");
+        $('h3').animate(
+            {'font-size': '1em', 'margin-left': '0px'},
+            2000,
+        );
+    }
+    
+    $('h3').animate(
+        {'font-size': '3em', 'margin-left': '100px'},
+        2000,
+        poZakonczonejAnimacji
+    );
+
+})
