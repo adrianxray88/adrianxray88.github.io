832ca00dc34749ae6c10b7b254047ae106b9f2b4
Adrian Lewandowski <lewandow.adr88@gmail.com>
2019-07-11 10:25:43 +0200

    Update main.js

--- Powtórka/js/Pętle/js/main.js
+++ Powtórka/js/Pętle/js/main.js
@@ -21,3 +21,15 @@ do {
     console.log(iterator);
     iterator++;
 } while(iterator < 0);
+
+const iloczynKwadratow = (liczba1, liczba2) => {
+    let kwadrat1 = Math.pow(liczba1, 2);
+    let kwadrat2 = liczba2 * liczba2;
+    return kwadrat1 * kwadrat2;
+
+}
+let pierwszaLiczba = 5;
+let drugaLiczba = 4;
+let wynikDzialaniaFunkcji = iloczynKwadratow(pierwszaLiczba, drugaLiczba);
+
+console.log(wynikDzialaniaFunkcji)
