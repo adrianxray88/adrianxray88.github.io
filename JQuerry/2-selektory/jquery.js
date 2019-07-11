11a882d7ba065315668685c45f3cdbdbc1534bac
Adrian Lewandowski <lewandow.adr88@gmail.com>
2019-07-11 11:24:47 +0200

    Create jquery.js

---
+++ JQuerry/2-selektory/jquery.js
@@ -0,0 +1,10 @@
+$(function(){
+    'use strict';
+
+    let paragraphOne = $('#paragraf');
+    console.log(paragraphOne);
+    jQuery('#paragraf').css({"background": "red"}).hide('slow').show(2000);
+    let paragraphTwo = $('id');
+    console.log(paragraphTwo);
+})
+
