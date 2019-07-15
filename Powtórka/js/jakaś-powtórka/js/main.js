221ce3186666a7d48eabb3fb68105598f843c058
Adrian Lewandowski <lewandow.adr88@gmail.com>
2019-07-15 10:54:58 +0200

    Update main.js

--- Powtórka/js/jakaś-powtórka/js/main.js
+++ Powtórka/js/jakaś-powtórka/js/main.js
@@ -1,14 +1,26 @@
+/* Liczenie wskaznika BMI */
+const countBmi = (weight, height) => {
+    return weight/Math.pow(height, 2);
+}
+
 /*obsługa zdarzenia i pobranie danych*/
 
-document.getElementByID('bmi-form').addEventlistener('submit', e => {
+document.getElementById('bmi-form').addEventListener('submit', e => {
     e.preventDefault();
     const nameInput = document.getElementById('bmi-name').value;
-    const weightInput = document.getElementById('bmi-weight').value;
-    const heightInput = document.getElementById('bmi-height').value;
+    const weightInput = parseFloat(document.getElementById('bmi-weight').value);
+    const heightInput = parseFloat(document.getElementById('bmi-height').value)/100;
+    
+    
+    const result = countBmi(weightInput, heightInput);
     console.log(nameInput);
     console.log(weightInput);
     console.log(heightInput);
+    console.log(result);
+    document.getElementById('bmi-result').innerHTML = 'Cześć ${nameInput}, Twój wskaźnik BMI to ${result}'
+
 });
+
 
 // function() {}
 /* a => a*a; */
